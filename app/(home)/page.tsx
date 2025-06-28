021ac01a0a9cd93dea68c9f59525e91918a46547
JP Mondal <jpmondal2003@gmail.com>
2025-06-28 22:12:12 +0600

    Navbar running

---
+++ app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/home/header/navbar";
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Page;
---
+++ components/home/header/navbar.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Button } from "../../ui/button";
+import Link from "next/link";
+import SearchInput from "./search-input";
+import ToggleMode from "./toggle-mode";
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 w-full z-50 border border-b backdrop-blur bg-background/95 supports-[backdrop-filter]:bg-background/60">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          {/* Left Section  */}
+          <div className="flex items-center">
+            <Link href="/">
+              <span className="font-bold text-2xl">
+                <span className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent dark:from-purple-400 dark:via-pink-300 dark:to-indigo-400">
+                  Blog
+                </span>
+                <span className="text-foreground">Box</span>
+              </span>
+            </Link>
+          </div>
+
+          {/* Desktop Menu  */}
+          <div className="hidden md:flex items-center gap-4">
+            <Link
+              className="text-sm font-medium text-foreground hover:text-blue-700 transition-colors"
+              href="/articles"
+            >
+              Article
+            </Link>
+            <Link
+              className="text-sm font-medium text-foreground hover:text-blue-700 transition-colors"
+              href="/tutorials"
+            >
+              Tutorial
+            </Link>
+            <Link
+              className="text-sm font-medium text-foreground hover:text-blue-700 transition-colors"
+              href="/about"
+            >
+              About
+            </Link>
+            <Link
+              className="text-sm font-medium text-foreground hover:text-blue-700 transition-colors"
+              href="/dashboard"
+            >
+              Dashboard
+            </Link>
+          </div>
+
+          {/* Right Section  */}
+          <div>
+            <SearchInput />
+            <ToggleMode />
+            <div className="hidden md:flex items-center gap-2">
+              <Button>Login</Button>
+              <Button>Sign Up</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
